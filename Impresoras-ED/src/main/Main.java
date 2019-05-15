8a5dcbbd2353cb13dcc9ce957199fb67867f3f3b
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2019-05-15 12:11:53 -0500

    Update Main.java

--- Impresoras-ED/src/main/Main.java
+++ Impresoras-ED/src/main/Main.java
@@ -1,45 +1,86 @@
-//Trabajo Realizado por: AndrÈs Guillermo Bonilla Olarte
 package main;
 import java.io.*;
 import java.util.LinkedList;
 import impresoras.Impresora1;
 import impresoras.Impresora2;
-
+/**
+ * ED - Trabajo Impresoras
+ * @author Andr√©s Guillermo Bonilla Olarte
+ * 13 - 05 - 2019
+ */
 public class Main 
 {  
-    private static Thread  hilo1  = new  Impresora1(); //CreaciÛn cola de impresiÛn 1
-    private static Thread  hilo2  = new  Impresora2(); //CreaciÛn cola de impresiÛn 2
-	//private static Impresora1 imprimir1 = new Impresora1();
-	//private static Impresora2 imprimir2 = new Impresora2();
+    private static Thread  hilo1  = new  Impresora1(); //Creaci√≥n cola de impresi√≥n 1
+    private static Thread  hilo2  = new  Impresora2(); //Creaci√≥n cola de impresi√≥n 2
     private static LinkedList<String> cola1 = new LinkedList<String>();
     private static LinkedList<String> cola2 = new LinkedList<String>();
     private static File archivo = null;
     private static FileReader fr = null;
     private static BufferedReader br = null;
+    private static int tiempo_colaImp1 = 0;
+    private static int tiempo_colaImp2 = 0;
     
     public static void main(String[] args) 
     {
         try
         {
-        	String renglon; //Ac· se guardar· cada renglÛn del archivo de texto
+        	String renglon; //Ac√° se guardar√° cada rengl√≥n del archivo de texto
         	String [] temporal_archivo; //Archivo de texto completo
         	
-	        archivo = new File ("C:\\Users\\RGH-LS13-08\\Downloads\\Impresoras-ED\\ArchivoImpresion.txt"); //UbicaciÛn del archivo
+	        archivo = new File ("D:\\Eclipse\\eclipse\\Impresoras-ED\\ArchivoImpresion.txt"); //Ubicaci√≥n del archivo
 	        fr = new FileReader (archivo); //Guardar el archivo en fr
 	        br = new BufferedReader(fr); //Leer fr
 	        
         	while((renglon = br.readLine()) != null) //Mientras el renglon del archivo, tenga algo
         	{
-        		temporal_archivo = renglon.split(" "); //RenglÛn, separarlo por espacio
+        		temporal_archivo = renglon.split(" "); //Rengl√≥n, separarlo por espacio
         		
 				if(temporal_archivo [0].equalsIgnoreCase("ADMON") || temporal_archivo [0].equalsIgnoreCase("GERENCIA")) 
 				{
-					cola1.add(renglon);//AÒadimos el renglÛn, a la cola de la impresora
-				}else 
+					switch(Integer.parseInt(temporal_archivo[1]))
+					{
+						case 1:
+							tiempo_colaImp1++; //Tiempo acumulado de cola: 1 minuto 
+							break;
+							
+						case 2:
+							tiempo_colaImp1+=3; //Tiempo acumulado de cola: 3 minutos
+							break;
+							
+						case 3:
+							tiempo_colaImp1+=5; //Tiempo acumulado de cola: 5 minutos
+							break;
+					}
+					cola1.add(renglon);
+				}
+       
+				else 
 				{
 					if(temporal_archivo [0].equalsIgnoreCase("MERCADEO") || temporal_archivo [0].equalsIgnoreCase("PRODUC"))
 					{
-						cola2.add(renglon);//AÒadimos el renglÛn, a la cola de la impresora
+						switch(Integer.parseInt(temporal_archivo[1]))
+						{
+							case 1:
+								tiempo_colaImp2++; //Tiempo acumulado de cola: 1 minuto 
+								break;
+								
+							case 2:
+								tiempo_colaImp2+=3; //Tiempo acumulado de cola: 3 minutos
+								break;
+								
+							case 3:
+								tiempo_colaImp2+=5; //Tiempo acumulado de cola: 5 minutos
+								break;
+						}
+						if(cola2.size() > 2) //L√≠mite de archivos de la cola 2 = 2 Archivos
+						{
+							System.out.println("Cola de impresi√≥n 2, llena. Por favor, espere a que termine de imprimirse un archivo");
+						}
+						else
+						{
+							cola2.add(renglon);//A√±adimos el rengl√≥n, a la cola de la impresora
+						}
+						
 					}
 				}
 			}
@@ -47,19 +88,19 @@ public class Main
 			
 			System.out.println("Bienvenido al Administrador de Impresiones MinuTech");
 			System.out.println("Departamentos:\n"
-							 + "1. AdministraciÛn\n"
+							 + "1. Administraci√≥n\n"
 							 + "2. Gerencia\n"
 							 + "3. Mercadeo\n"
-							 + "4. ProducciÛn");
+							 + "4. Producci√≥n");
 			System.out.println("\nCola de Impresora 1: " + cola1 +"\n"
 							   + "Cola de Impresora 2: " + cola2 + "\n");
 			
         	
-        	Impresora1.setQueue(cola1); //Set de la cola de la impresora 1
-			Impresora2.setQueue(cola2); //Set de la cola de la impresora 2
+        	Impresora1.setQueue(cola1);
+			Impresora2.setQueue(cola2);
 			
-			hilo1.start(); //Inicio al hilo 1
-			hilo2.start(); //Inicio al hilo 2
+			hilo1.start();
+			hilo2.start();
 			
 				
         }
